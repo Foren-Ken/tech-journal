a2f0135149cd54b628753d011fa08dd46aa8bd3e
Ken <143446633+Foren-Ken@users.noreply.github.com>
2025-01-23 12:16:15 -0500

    Update multiping.sh

--- SEC-335/multiping.sh
+++ SEC-335/multiping.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-touch sweep1.txt
 for ip in $(seq 2 50);
 do 
         static="10.0.5."
         full=$static$ip
 
         if ping -c 1 -W 1 $full; 
-                then echo $full >> sweep1.txt
+                then echo $full > sweep1.txt
         fi
 done
